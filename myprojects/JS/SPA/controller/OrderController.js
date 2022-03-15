8c9ccbe96637624917b4245ba460ae5df22558e3
HansiHashani0415 <hansihashani824@gmail.com>
2022-03-15 16:29:18 +0530

    Added validation in order form

--- myprojects/JS/SPA/controller/OrderController.js
+++ myprojects/JS/SPA/controller/OrderController.js
@@ -25,16 +25,143 @@ $("#btnAddToCart").click(function () {
     }
 });
 
+$('#txtOrderID,#txtCash,#txtDiscount,#txtBalance').on('blur', function () {
+    formValidOrder();
+});
+
+
+$("#btnSaveItem").attr('disabled', true);
+
+const regExOrderID = /^O[0-9]{3,4}$/;
+const regExDiscount = /^[0-9]{1,}$/;
+
+function formValidOrder() {
+    var orderID = $("#txtOrderID").val();
+    $("#txtOrderID").css('border', '2px solid #ced4da');
+    if (regExOrderID.test(orderID)) {
+        var cash = $("#txtCash").val();
+        if (regExUnitPrice.test(cash)){
+            $("#txtCash").css('border', '2px solid #ced4da');
+            var discount = $("#txtDiscount").val();
+            if (regExDiscount.test(discount)){
+                $("#txtDiscount").css('border', '2px solid #ced4da');
+                var balance = $("#txtBalance").val();
+                if (regExDiscount.test(balance)){
+                    $("#txtBalance").css('border', '2px solid #ced4da');
+                    $("#btnSaveItem").attr('disabled', false);
+                    return true;
+                }else{
+                    $("#txtBalance").css('border', '2px solid red');
+                    return false;
+                }
+            }else{
+                $("#txtDiscount").css('border', '2px solid red');
+                return false;
+            }
+        }else{
+            $("#txtCash").css('border', '2px solid red');
+            return false;
+        }
+    } else {
+        $("#txtOrderID").css('border', '2px solid red');
+        return false;
+    }
+
+}
+function checkIfValidOrder() {
+    var oId = $("#txtOrderID").val();
+    if (regExOrderID.test(oId)) {
+        $("#txtCash").focus();
+        var c = $("#txtCash").val();
+        if (regExUnitPrice.test(c)) {
+            $("#txtDiscount").focus();
+            var disc = $("#txtDiscount").val();
+            if (regExDiscount.test(disc)) {
+                $("#txtBalance").focus();
+                var balanc = $("#txtBalance").val();
+                var resp = regExDiscount.test(balanc);
+                if (resp) {
+
+                    // let res = confirm("Do you really want to save this customer..?");
+
+                    //clearAll();
+                    return true;
+                } else {
+                    $("#txtBalance").focus();
+                }
+            } else {
+                $("#txtDiscount").focus();
+            }
+        } else {
+            $("#txtCash").focus();
+        }
+    } else {
+        $("#txtOrderID").focus();
+    }
+}
+function setButtonOrder() {
+
+    let b = formValidOrder();
+    if (b) {
+        $("#btnConfirmOrder").attr('disabled', false);
+    } else {
+        $("#btnConfirmOrder").attr('disabled', true);
+    }
+}
 
 $("#btnConfirmOrder").click(function () {
-    var list = new Array();
+
+   var list = new Array();
     for (let cartDBElement of cartDB) {
         list.push(cartDBElement);
     }
     var newOrder = new OrderDTO($("#txtOrderID").val(),$("#txtCusIdInOrder").val(),$("#txtDate").val(),"10:14",$("#total").text(),list);
-    saveOrder(newOrder);
-    clearDetailsInPlaceOrder();
+    if (checkIfValidOrder()){
+        saveOrder(newOrder);
+        clearDetailsInPlaceOrder();
+    }
+
 });
+$('#btnSaveCustomer').click(function () {
+    checkIfValidOrder();
+});
+
+$("#txtOrderID").on('keyup', function (eventOb) {
+    setButtonOrder();
+    if (eventOb.key == "Enter") {
+        checkIfValidOrder();
+    }
+
+    if (eventOb.key == "Control") {
+        var typedCustomerID = $("#txtOrderID").val();
+        var srcCustomer = searchCustomer(typedCustomerID);
+        // $("#txtCusID").val(srcCustomer.getCustomerID());
+        // $("#txtCusName").val(srcCustomer.getCustomerName());
+        // $("#txtCusAddress").val(srcCustomer.getCustomerAddress());
+        // $("#txtCusTP").val(srcCustomer.getCustomerSalary());
+    }
+});
+$("#txtCash").on('keyup', function (eventOb) {
+    setButtonOrder();
+    if (eventOb.key == "Enter") {
+        checkIfValidOrder();
+    }
+});
+
+$("#txtDiscount").on('keyup', function (eventOb) {
+    setButtonOrder();
+    if (eventOb.key == "Enter") {
+        checkIfValidOrder();
+    }
+});
+
+$("#txtBalance").on('keyup', function (eventOb) {
+    setButtonOrder();
+    if (eventOb.key == "Enter") {
+        checkIfValidOrder();
+    }
+});
+
 
 $("#btnCancelOrder").click(function () {
      clearDetailsInPlaceOrder();
